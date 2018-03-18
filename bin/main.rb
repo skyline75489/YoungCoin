3cf136a156151c565d7b17349a2a0463ca4a6d53
skyline75489 <skyline75489@outlook.com>
2018-03-18 09:11:31 +0800

    fix

--- bin/main.rb
+++ bin/main.rb
@@ -1,9 +1,10 @@
 require 'pp'
 require 'json'
-require_relative '../lib/YoungCoin/blockchain'
+require_relative '../lib/YoungCoin'
 
 chain = YoungCoin::BlockChain.new()
 
 b1 = chain.generate_next_block('d1')
-chain.add_block(b1)
+b2 = chain.generate_next_block('d2')
+
 pp chain
--- lib/YoungCoin.rb
+++ lib/YoungCoin.rb
@@ -1,2 +1,3 @@
 require_relative 'YoungCoin/block'
 require_relative 'YoungCoin/transaction'
+require_relative 'YoungCoin/blockchain'
